5f90d41a9d0b8b473f3fc0e6efe9a17da49b86af
Diogo Alves <diogomoura2014@gmail.com>
2021-04-30 15:08:21 +0100

    feat: added flip image horizontally

--- pixel.h
+++ pixel.h
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdlib.h>
 typedef struct
 {
   int red;
@@ -15,41 +16,65 @@ typedef struct
   PIXEL **matrix;
 } IMAGE;
 
-
-
-IMAGE setImage(char *magicNumber, int height, int width, int maxValue){
+IMAGE setImage(char *magicNumber, int height, int width, int maxValue)
+{
   IMAGE img;
   img.magicNumber = magicNumber;
   img.height = height;
   img.width = width;
   img.maxValue = maxValue;
-  img.matrix = malloc(width*sizeof(PIXEL*));
+  img.matrix = malloc(width * sizeof(PIXEL *));
   for (int i = 0; i < width; i++)
   {
-    img.matrix[i] = malloc(width*sizeof(PIXEL));
+    img.matrix[i] = malloc(height * sizeof(PIXEL));
   }
   return img;
 }
 
-void printImage(IMAGE img){
-  printf("%s\n",img.magicNumber);
+void printImage(IMAGE img)
+{
+  printf("%s\n", img.magicNumber);
   printf("%d %d\n", img.width, img.height);
-  printf("%d\n",img.maxValue);
+  printf("%d\n", img.maxValue);
   for (int i = 0; i < img.height; i++)
   {
     for (int j = 0; j < img.width; j++)
     {
-      printf("%d %d %d\n", img.matrix[j][i].red, img.matrix[j][i].green,img.matrix[j][i].blue);
+      printf("%d %d %d\n", img.matrix[j][i].red, img.matrix[j][i].green, img.matrix[j][i].blue);
     }
   }
-  
-
 }
 
-PIXEL setPixel(int r, int g, int b){
+PIXEL setPixel(int r, int g, int b)
+{
   PIXEL p;
   p.red = r;
   p.green = g;
   p.blue = b;
   return p;
+}
+void readImageFile(IMAGE img, FILE *file)
+{
+  for (int i = 0; i < img.height; i++)
+  {
+    for (int j = 0; j < img.width; j++)
+    {
+      int r, g, b;
+      fscanf(file, "%d %d %d", &r, &g, &b);
+      img.matrix[j][i] = setPixel(r, g, b);
+    }
+  }
+}
+void readImage(IMAGE img)
+{
+  int r, g, b;
+  for (int i = 0; i < img.height; i++)
+  {
+    for (int j = 0; j < img.width; j++)
+    {
+      int r, g, b;
+      scanf("%d %d %d", &r, &g, &b);
+      img.matrix[j][i] = setPixel(r, g, b);
+    }
+  }
 }
--- ppm_h_flip.c
+++ ppm_h_flip.c
@@ -2,36 +2,31 @@
 #include <stdlib.h>
 #include "pixel.h"
 
+void flip(IMAGE img, IMAGE newImg)
+{
+  for (int i = 0; i < img.height; i++)
+  {
+    for (int j = img.width; j > 0; j--)
+    {
+      newImg.matrix[img.width - j][i] = img.matrix[j - 1][i];
+    }
+  }
+}
 int main(int argc, char const *argv[])
 {
-  int l, c, i, j;
+
+  int height = 0, width = 0;
   int maxValue = 0;
+  char header[4];
   FILE *file;
   file = fopen(argv[1], "r+");
-  char header[4];
   fscanf(file, "%s", header);
-  int height = 0, width = 0;
   fscanf(file, "%d %d", &width, &height);
   fscanf(file, "%d", &maxValue);
   IMAGE img = setImage(header, height, width, maxValue);
   IMAGE newImg = setImage(header, height, width, maxValue);
-  PIXEL newImg1[width][height];
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = 0; j < width; j++)
-    {
-      int r, g, b;
-      fscanf(file, "%d %d %d", &r, &g, &b);
-      img.matrix[j][i] = setPixel(r, g, b);
-    }
-  }
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = width; j > 0; j--)
-    {
-      newImg.matrix[width - j][i] = img.matrix[j - 1][i];
-    }
-  }
+  readImageFile(img, file);
+  flip(img, newImg);
   printImage(newImg);
   fclose(file);
   return 0;
