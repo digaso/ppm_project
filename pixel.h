4cd6dd700315edb67d7c613bd8f80f1a7c17300a
Diogo Alves <diogomoura2014@gmail.com>
2021-05-01 16:19:11 +0100

    feat: added QoL functions to read and print

--- pixel.h
+++ pixel.h
@@ -32,20 +32,6 @@ IMAGE setImage(char *magicNumber, int height, int width, int maxValue)
   return img;
 }
 
-void printImage(IMAGE img)
-{
-  printf("%s\n", img.magicNumber);
-  printf("%d %d\n", img.width, img.height);
-  printf("%d\n", img.maxValue);
-  for (int i = 0; i < img.height; i++)
-  {
-    for (int j = 0; j < img.width; j++)
-    {
-      printf("%d %d %d\n", img.matrix[j][i].red, img.matrix[j][i].green, img.matrix[j][i].blue);
-    }
-  }
-}
-
 PIXEL setPixel(int r, int g, int b)
 {
   PIXEL p;
@@ -77,5 +63,84 @@ void readImage(IMAGE img)
       scanf("%d %d %d", &r, &g, &b);
       img.matrix[j][i] = setPixel(r, g, b);
     }
+  }
+}
+
+IMAGE readIO()
+{
+  IMAGE img;
+  int maxValue, height, width;
+  char header[3];
+  scanf("%s", header);
+  scanf("%d %d", &width, &height);
+  scanf("%d", &maxValue);
+  img = setImage(header, height, width, maxValue);
+  readImage(img);
+  return img;
+}
+IMAGE readFile(FILE *file)
+{
+
+  IMAGE img;
+  int maxValue, height, width;
+  char header[3];
+  fscanf(file, "%s", header);
+  fscanf(file, "%d %d", &width, &height);
+  fscanf(file, "%d", &maxValue);
+  img = setImage(header, height, width, maxValue);
+  readImageFile(img, file);
+  return img;
+    
+}
+IMAGE scanImage(int argc, char const *argv[])
+{
+  FILE *file;
+  IMAGE img;
+  if (argc > 1)
+  {
+    file = fopen(argv[1], "r+");
+    img = readFile(file);
+    fclose(file);
+  }
+  else if (argc == 1)
+  {
+    img = readIO();
+  }
+  return img;
+}
+
+void printToFile(IMAGE img, FILE *file){
+  fprintf(file ,"%s\n", img.magicNumber);
+  fprintf(file ,"%d %d\n", img.width, img.height);
+  fprintf(file ,"%d\n", img.maxValue);
+  for (int i = 0; i < img.height; i++)
+  {
+    for (int j = 0; j < img.width; j++)
+    {
+      fprintf(file,"%d %d %d\n", img.matrix[j][i].red, img.matrix[j][i].green, img.matrix[j][i].blue);
+    }
+  }
+}
+void printImageIO(IMAGE img)
+{
+  printf("%s\n", img.magicNumber);
+  printf("%d %d\n", img.width, img.height);
+  printf("%d\n", img.maxValue);
+  for (int i = 0; i < img.height; i++)
+  {
+    for (int j = 0; j < img.width; j++)
+    {
+      printf("%d %d %d\n", img.matrix[j][i].red, img.matrix[j][i].green, img.matrix[j][i].blue);
+    }
+  }
+}
+void printImage(int argc, char const *argv[], IMAGE img){
+  FILE *file;
+  if(argc > 2){
+    file = fopen(argv[2], "w");
+    printToFile(img, file);
+    fclose(file);
+  }else{
+    printImageIO(img);
   }
 }
