e8414c3da171eb026fa0d3e96ea85050ea53bdc8
Diogo Alves <diogomoura2014@gmail.com>
2021-05-03 17:38:49 +0100

    feat: added rotate right function

---
+++ ppm_rot_right.c
@@ -0,0 +1,22 @@
+#include "pixel.h"
+
+void rotate(IMAGE img, IMAGE newImg)
+{
+  for (int i = 0; i < newImg.height; i++)
+  {
+    for (int j = 0; j < newImg.width; j++)
+    {
+      newImg.matrix[j][i] = img.matrix[i][newImg.width - j];
+    }
+  }
+}
+
+int main(int argc, char const *argv[])
+{
+  IMAGE img = scanImage(0, argc, argv);
+  IMAGE newImg = setImage(img.magicNumber, img.width, img.height, img.maxValue);
+  rotate(img, newImg);
+  printImage(0, argc, argv, newImg);
+
+  return 0;
+}
